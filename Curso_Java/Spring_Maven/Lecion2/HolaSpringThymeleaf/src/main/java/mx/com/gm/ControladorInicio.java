a1af6143f92384edf6814b7ffc560b9190b6b093
Ollin Gomez <tonatiuh.ollin.gomez@gmail.com>
2022-09-13 13:47:19 -0500

    ADD: Creación lista Personas

--- Curso_Java/Spring_Maven/Lecion2/HolaSpringThymeleaf/src/main/java/mx/com/gm/ControladorInicio.java
+++ Curso_Java/Spring_Maven/Lecion2/HolaSpringThymeleaf/src/main/java/mx/com/gm/ControladorInicio.java
@@ -1,5 +1,7 @@
 package mx.com.gm;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import lombok.extern.slf4j.Slf4j;
 import mx.com.gm.domain.Persona;
 import org.springframework.beans.factory.annotation.Value;
@@ -18,16 +20,31 @@ public class ControladorInicio {
     @GetMapping("/")
     public String inicio(Model model){
         var mensaje = "Mensaje con Thymeleaf";
+        
         var persona = new Persona();
         persona.setNombre("Juan");
         persona.setApellido("Perez");
         persona.setEmail("jperez@mail.com");
         persona.setTelefono("55443322");
-               
+        
+        var persona2 = new Persona();
+        persona2.setNombre("Karla");
+        persona2.setApellido("Gomez");
+        persona2.setEmail("kgomez@mail.com");
+        persona2.setTelefono("13229900");
+        
+//       var personas = new ArrayList();
+//       personas.add(persona);
+//       personas.add(persona2);
+
+        var personas = Arrays.asList(persona,persona2);
+ //       var personas = new ArrayList();
+        
         log.info("ejecutando el controlador Spring MVC");
         model.addAttribute("mensaje",mensaje);
         model.addAttribute("saludo",saludo);
-        model.addAttribute("persona", persona);
+//        model.addAttribute("persona", persona);
+        model.addAttribute("personas", personas);
         return "index";
     }
 }
